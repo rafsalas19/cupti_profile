c4e836e68dc5b315d7956ee47340058be40eee87
Ubuntu <rafsalas@wavvmssjf000000.sfeh4gmhcxgenklyd3lehlekyd.bx.internal.cloudapp.net>
2022-03-25 18:23:42 +0000

    fixed compile errors

--- include/cuptiMetrics.h
+++ include/cuptiMetrics.h
@@ -3,20 +3,22 @@
 #include <map>
 #include <stdlib.h>
 
+using namespace std;
+
 namespace Metrics{
-	typedef std::map<string, int> metricList;
+	typedef std::map<int, string> metricList;
 
 	metricList get_metricList(){
 		metricList metrics=
 		{
 		        {1, "sm__warps_active.avg.pct_of_peak_sustained_active"},
         		{2, "dram__bytes_read.sum.per_second"},
-        		{3, "smsp__thread_inst_executed_per_inst_executed.ratio"}
+        		{3, "smsp__thread_inst_executed_per_inst_executed.ratio"},
 		
-		}
+		};
 
 
-		return metricList;
+		return metrics;
 	}
 }
 
